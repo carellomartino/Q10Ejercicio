4f11d4aa60759b5bb1a48a5e725297e97187f02e
Nicolas <carellomartino@gmail.com>
2019-08-07 21:02:11 -0300

    starting

--- Código.js
+++ Código.js
@@ -1,18 +1,34 @@
-function HelloWorld() 
-{
-  var ui = SpreadsheetApp.getUi()
-
-  ui.alert("Hola Mundo!!!!");
-};
-
 function onOpen() {
   var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
   var entries = [{
     name : "Calcular Costos",
-    functionName : "HelloWorld"
+    functionName : "calcularCostos"
   },{
     name: "Enviar Listado",
-    functionName: "LALA"
+    functionName: "enviarListado"
   }];
   spreadsheet.addMenu("Menu Ejercicio", entries);
 };
+
+
+
+function calcularCostos() 
+{
+  var ui = SpreadsheetApp.getUi()
+
+  ui.alert("Hola Mundooooo Feo!!!!");
+};
+
+
+function enviarListado()
+{
+  var ui = SpreadsheetApp.getUi()
+  var productId = SpreadsheetApp.getActiveSheet().getRange("A2:A6").getValues()
+  //aca tengo los ids de productos, ahora tengo que leer las rows q tengan ese id de producto
+
+
+
+
+  ui.alert(productId);
+};
+
